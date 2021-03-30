4cdf21d8cfa5657616227e256b68eafed9c1f613
elmernocon <elmernocon@gmail.com>
2021-03-30 22:51:03 +0800

    UPM Release 0.1.0

---
+++ Runtime/Greeter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+namespace UPMPackageDemo
+{
+    public class Greeter : MonoBehaviour
+    {
+        private IEnumerator Start()
+        {
+            const int NO_OF_GREETINGS = 10;
+            const float DELAY_PER_GREETING = 0.1f;
+
+            var waitForSeconds = new WaitForSeconds(DELAY_PER_GREETING);
+
+            for (var i = 0; i < NO_OF_GREETINGS; ++i)
+            {
+                yield return waitForSeconds;
+                Greet();
+            }
+        }
+
+        private void Greet()
+        {
+            Debug.Log($"Hello!");
+        }
+    }
+}
